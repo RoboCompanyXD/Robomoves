0bfac5f8fa97973b10aaa429190da85b6f3413fe
Xabi Garde <xabier.gardeazabal@ehu.eus>
2020-06-19 13:29:41 +0200

    Estructurar directorios y reorganizar ficheros

---
+++ Lidar/Lidar.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Lidar.h
+ * Author: chris
+ *
+ * Created on 19 de junio de 2020, 13:05
+ */
+
+#ifndef LIDAR_H
+#define	LIDAR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* LIDAR_H */
+
