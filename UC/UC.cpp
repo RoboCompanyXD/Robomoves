719f13158d1445bae34a512a0c6285bb6a197de5
Christian <christi.an.martin.me.ri.no@gmail.com>
2020-07-01 14:14:15 +0200

    Completar Statechart
    - Añadir estados statechart

--- UC/UC.cpp
+++ UC/UC.cpp
@@ -1,8 +1,7 @@
 
 #include "UC.h"
 
-UC::UC()
-{
+UC::UC() {
     initStatechart();
 }
 
@@ -20,4 +19,62 @@ void UC::initStatechart() {
     CliffAhead_subState = OMNonState;
 }
 
-UC::statechart_process(){}
+UC::statechart_process() {
+
+    switch (rootState_active) {
+            // State Initializing
+            // Description: Estado de iniciacilación.
+            // Mediante la funcion Init se prepara todo el sistema.
+        case Initializing:
+        {
+
+            //## transition 1 
+            if (initStatus == 1) {
+
+                rootState_subState = Idle;
+                rootState_active = Idle;
+
+            } else if (initStatus > 1) {
+
+                rootState_subState = InitFailed;
+                rootState_active = InitFailed;
+
+            }
+            else if (check_btnSpot()) {
+
+                rootState_subState = Shutdown;
+                rootState_active = Shutdown;
+
+            }
+
+        }
+            break;
+            // State Idle
+            // Description: Modo de espera.
+            // Se pulsa el boton "Clean" para empezar con el modo normal de operación.
+            // El boton "Dock" o un nivel de batería inferior al 15% enviarán el robot a la base de carga.
+            // Una vez el robot está en la base de carga se retorna a este estado.
+            // En el modo de espera se puede estar cargando o sin cargar
+        case Idle:
+        {
+            if (sensoresBateria < 15 || check_btnDock()) {
+                
+                rootState_subState = Dock;
+                rootState_active = Dock;
+                //#[ state Dock.(Entry) 
+                gotoDock();
+                //#]
+                
+            } else if (check_btnClean()) {
+                res = IdleTakebtnClean();
+            } else if (check_btnSpot()) {
+                
+                rootState_subState = Shutdown;
+                rootState_active = Shutdown;
+               
+            
+        }
+            break;
+    }
+
+}
--- UC/UC.h
+++ UC/UC.h
@@ -8,56 +8,58 @@
 #ifndef UC_H
 #define	UC_H
 
-class UC{
+extern bool check_btnSpot();
+extern bool check_btnClean();
+extern bool check_btnDock();
+
+//## attribute BumpObstacle
+extern int BumpObstacle;
+
+//## attribute ComputeStepTime
+extern int ComputeStepTime;
+
+//## attribute cameraIsPersonInView
+extern bool cameraIsPersonInView;
+
+//## attribute initStatus
+extern int initStatus;
+
+//## attribute lidarIsObstable
+extern int lidarIsObstable;
+
+//## attribute sensoresBateria
+extern int sensoresBateria;
+
+//## attribute sensoresInDock
+extern int sensoresInDock;
+
+//## attribute sensoresLBumpFront
+extern bool sensoresLBumpFront;
+
+//## attribute sensoresLBumpSide
+extern bool sensoresLBumpSide;
+
+//## attribute sensoresSumAngulo
+extern int sensoresSumAngulo;
+
+//## attribute sensoresSumDistancia
+extern int sensoresSumDistancia;
+
+class UC {
     UC();
     ~UC();
 
 public:
-    
+
     void statechart_process();
-    
-    inline bool rootState_IN() const;
-    inline bool UnDock_IN() const;
-    inline bool UnDock_Rotate180_IN() const;
-    inline bool ExitDock_IN() const;
-    inline bool EndUndoParkHelperState_IN() const;
-    inline bool Shutdown_IN() const;
-    inline bool NormalOperate_IN() const;
-    inline bool TrackingByCamera_IN() const;
-    inline bool PersonOutView_IN() const;
-    inline bool PersonOutView_RotateToMove_IN() const;
-    inline bool PersonOutView_Rotate360_IN() const;
-    inline bool PersonOutView_GoForward_IN() const;
-    inline bool PersonOutView_ComputePosition_IN() const;
-    inline bool PersonInView_IN() const;
-    inline bool PersonInView_PathBlocked_IN() const;
-    inline bool PersonInView_ApproachUser_IN() const;
-    inline bool ComputeStepBlocked_IN() const;
-    inline bool ComputeStepApproach_IN() const;
-    inline bool DodgeObstacle_IN() const;
-    inline bool Dodge_MoveBack_IN() const;
-    inline bool CrashAlgorithm_IN() const;
-    inline bool CrashAlorithmEndHelperState_IN() const;
-    inline bool CrashAlgorithm_RecoverTrajectory_IN() const;
-    inline bool CrashAlgorithm_GoForwardExtended_IN() const;
-    inline bool CrashAlgorithm_GoForward_IN() const;
-    inline bool CrashAlgorithm_DodgeParallel_IN() const;
-    inline bool CrashAlgorithm_Dodge_IN() const;
-    inline bool CliffAhead_IN() const;
-    inline bool CliffAhead_Rotate180_IN() const;
-    inline bool CliffAhead_GoForward_IN() const;
-    inline bool Initializing_IN() const;
-    inline bool InitFailed_IN() const;
-    inline bool Idle_IN() const;
-    inline bool Dock_IN() const;
-    
+
 private:
 protected:
-    
+
     void initStatechart();
-    
+
     void cancelTimeouts();
-    
+
     enum UC_Enum {
         OMNonState = 0,
         UnDock = 1,
@@ -92,9 +94,10 @@ protected:
         Initializing = 30,
         InitFailed = 31,
         Idle = 32,
-        Dock = 33
+        Dock = 33,
+        Statechart_End = 1000
     };
-    
+
     int rootState_subState;
     
     int rootState_active;
@@ -107,155 +110,19 @@ protected:
     
     int PersonOutView_subState;
     
-    int PersonInView_timeout;
-    
     int PersonInView_subState;
+    
+    int PersonInView_timeout;
     
     int DodgeObstacle_subState;
     
     int CrashAlgorithm_subState;
     
     int CliffAhead_subState;
-
+    
+    
+    
 };
-
-inline bool UC::rootState_IN() const {
-    return true;
-}
-
-inline bool UC::UnDock_IN() const {
-    return rootState_subState == UnDock;
-}
-
-inline bool UC::UnDock_Rotate180_IN() const {
-    return UnDock_subState == UnDock_Rotate180;
-}
-
-inline bool UC::ExitDock_IN() const {
-    return UnDock_subState == ExitDock;
-}
-
-inline bool UC::EndUndoParkHelperState_IN() const {
-    return UnDock_subState == EndUndoParkHelperState;
-}
-
-inline bool UC::Shutdown_IN() const {
-    return rootState_subState == Shutdown;
-}
-
-inline bool UC::NormalOperate_IN() const {
-    return rootState_subState == NormalOperate;
-}
-
-inline bool UC::TrackingByCamera_IN() const {
-    return NormalOperate_subState == TrackingByCamera;
-}
-
-inline bool UC::PersonOutView_IN() const {
-    return TrackingByCamera_subState == PersonOutView;
-}
-
-inline bool UC::PersonOutView_RotateToMove_IN() const {
-    return PersonOutView_subState == PersonOutView_RotateToMove;
-}
-
-inline bool UC::PersonOutView_Rotate360_IN() const {
-    return PersonOutView_subState == PersonOutView_Rotate360;
-}
-
-inline bool UC::PersonOutView_GoForward_IN() const {
-    return PersonOutView_subState == PersonOutView_GoForward;
-}
-
-inline bool UC::PersonOutView_ComputePosition_IN() const {
-    return PersonOutView_subState == PersonOutView_ComputePosition;
-}
-
-inline bool UC::PersonInView_IN() const {
-    return TrackingByCamera_subState == PersonInView;
-}
-
-inline bool UC::PersonInView_PathBlocked_IN() const {
-    return PersonInView_subState == PersonInView_PathBlocked;
-}
-
-inline bool UC::PersonInView_ApproachUser_IN() const {
-    return PersonInView_subState == PersonInView_ApproachUser;
-}
-
-inline bool UC::ComputeStepBlocked_IN() const {
-    return PersonInView_subState == ComputeStepBlocked;
-}
-
-inline bool UC::ComputeStepApproach_IN() const {
-    return PersonInView_subState == ComputeStepApproach;
-}
-
-inline bool UC::DodgeObstacle_IN() const {
-    return NormalOperate_subState == DodgeObstacle;
-}
-
-inline bool UC::Dodge_MoveBack_IN() const {
-    return DodgeObstacle_subState == Dodge_MoveBack;
-}
-
-inline bool UC::CrashAlgorithm_IN() const {
-    return DodgeObstacle_subState == CrashAlgorithm;
-}
-
-inline bool UC::CrashAlorithmEndHelperState_IN() const {
-    return CrashAlgorithm_subState == CrashAlorithmEndHelperState;
-}
-
-inline bool UC::CrashAlgorithm_RecoverTrajectory_IN() const {
-    return CrashAlgorithm_subState == CrashAlgorithm_RecoverTrajectory;
-}
-
-inline bool UC::CrashAlgorithm_GoForwardExtended_IN() const {
-    return CrashAlgorithm_subState == CrashAlgorithm_GoForwardExtended;
-}
-
-inline bool UC::CrashAlgorithm_GoForward_IN() const {
-    return CrashAlgorithm_subState == CrashAlgorithm_GoForward;
-}
-
-inline bool UC::CrashAlgorithm_DodgeParallel_IN() const {
-    return CrashAlgorithm_subState == CrashAlgorithm_DodgeParallel;
-}
-
-inline bool UC::CrashAlgorithm_Dodge_IN() const {
-    return CrashAlgorithm_subState == CrashAlgorithm_Dodge;
-}
-
-inline bool UC::CliffAhead_IN() const {
-    return NormalOperate_subState == CliffAhead;
-}
-
-inline bool UC::CliffAhead_Rotate180_IN() const {
-    return CliffAhead_subState == CliffAhead_Rotate180;
-}
-
-inline bool UC::CliffAhead_GoForward_IN() const {
-    return CliffAhead_subState == CliffAhead_GoForward;
-}
-
-inline bool UC::Initializing_IN() const {
-    return rootState_subState == Initializing;
-}
-
-inline bool UC::InitFailed_IN() const {
-    return rootState_subState == InitFailed;
-}
-
-inline bool UC::Idle_IN() const {
-    return rootState_subState == Idle;
-}
-
-inline bool UC::Dock_IN() const {
-    return rootState_subState == Dock;
-}
-
-
 
 #endif	/* ROBOTIA_H */
 
